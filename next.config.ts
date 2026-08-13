489564b7e0bbfaa8a4ddfd161544ce8e3daf8184
santiago <santiagociaponi3@gmail.com>
2026-08-12 21:42:20 -0300

    commit segundo

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["*.ngrok-free.app", "*.ngrok.io", "192.168.*.*"],
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination:
+          "http://backend-cows-env.eba-q7dyzcfv.us-east-2.elasticbeanstalk.com/api/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
